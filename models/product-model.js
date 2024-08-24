e2ae4f55d67eac8d0d1974e0bd9238f72620182d
addygit123 <minecraftaddy123@gmail.com>
2024-08-24 17:12:07 +0530

    initial

---
+++ models/product-model.js
@@ -0,0 +1,13 @@
+const productSchema = mongoose.Schema({
+    image:String,
+    name:String,
+    price:Number,
+    discount:{
+        type:Number,
+        default:0,
+    },
+    bgcolor:String,
+    panelcolor:String,
+    textcolor:String,
+});
+module.exports= mongoose.model("products",productSchema);
